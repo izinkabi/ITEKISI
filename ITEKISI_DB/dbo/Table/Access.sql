36de03ef6448db5d2edc0aadd6e2469d7c248486
ZingisaniMajama <215016842@student.uj.ac.za>
2020-02-11 18:42:47 +0200

    Updates on Table Access and Street relationships

--- ITEKISI_DB/dbo/Table/Access.sql
+++ ITEKISI_DB/dbo/Table/Access.sql
@@ -3,5 +3,5 @@
 	[Id] INT NOT NULL PRIMARY KEY, 
     [Type] VARCHAR(50) NULL, 
     [Level] VARCHAR(50) NULL, 
-    [ActivationId] INT NULL FOREIGN KEY REFERENCES Province(Id)
+    [ActivationId] INT NULL FOREIGN KEY REFERENCES Activation(Id)
 )
