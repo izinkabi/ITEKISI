f3f3b0433a027ba43a951155b9b5072481ea4d5a
Nkosinathi Nkosi <nkosinathi.vodacom@gmail.com>
2020-02-12 11:32:14 +0200

    New project added with a class library for data access from DB

---
+++ ITEKISI_DATA_LIBRARY/SqlDataAccess.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Configuration;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+
+namespace ITEKISI_DATA_LIBRARY
+{
+    class SqlDataAccess
+    {
+
+        public string GetConnectionString(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+        public List<T> LoadData<T, U>(string storedProcedure, U parameters, string connectionStringName)
+        {
+            string connectionString = GetConnectionString(connectionStringName);
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                List<T> rows = connection.Query<T>(storedProcedure, parameters, commandType: CommandType.StoredProcedure).ToList();
+                return rows;
+            }
+        }
+
+        public List<T> SaveData<T>(string storedProcedure, T parameters, string connectionStringName)
+        {
+            string connectionString = GetConnectionString(connectionStringName);
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+               connection.Execute(storedProcedure, parameters, commandType: CommandType.StoredProcedure)
+                
+            }
+        }
+    }
+}
